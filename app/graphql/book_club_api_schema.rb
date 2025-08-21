d84ef52afd7ba804d9f8a658e778894d9f753a61
Devesh Mhatre <devesh@incubyte.co>
2025-08-21 17:37:59 +0530

    Handle exceptions in case of record not found

--- app/graphql/book_club_api_schema.rb
+++ app/graphql/book_club_api_schema.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class BookClubApiSchema < GraphQL::Schema
+  rescue_from(ActiveRecord::RecordNotFound) do |err, obj, args, ctx, field|
+    raise GraphQL::ExecutionError, "Record not found: #{err.message}"
+  end
+
+  rescue_from(StandardError) do |err, obj, args, ctx, field|
+    Rails.logger.error(err.message)
+    Rails.logger.error(err.backtrace.join("\n"))
+    GraphQL::ExecutionError.new("Internal server error")
+  end
+
   use GraphQL::Batch
   query Types::QueryType
   mutation Types::MutationType
