e34c107fd8bb426a744a08a9f6601624fe85e83e
Devesh Mhatre <devesh@incubyte.co>
2025-08-29 11:55:00 +0530

    [RED] Add a test for empty title when creating an event

--- spec/graphql/create_event_spec.rb
+++ spec/graphql/create_event_spec.rb
@@ -32,5 +32,22 @@ RSpec.describe Mutations::CreateEvent do
         expect(result[:errors]).to be_empty
       end
     end
+
+    context "when the title is empty" do
+      let(:empty_title) { "" }
+      let(:description) { "To discuss the Clean Code book, duh" }
+      let(:location) { "Mumbai" }
+      let(:starts_at) { Faker::Time.forward(days: 1, period: :morning) }
+      let(:ends_at) { Faker::Time.forward(days: 1, period: :evening) }
+      let(:user_id) { @user.id }
+      let(:args) { { title: empty_title, description:, location:, starts_at:, ends_at:, user_id: } }
+
+      it "returns a validation error about title being empty" do
+        result = mutation.resolve(**args)
+
+        expect(result[:event]).to be_nil
+        expect(result[:errors]).to include("Title cannot be empty")
+      end
+    end
   end
 end
